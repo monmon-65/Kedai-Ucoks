dad0a926ae3bc0a0de2f0658aad708497c7dedf2
monmon-65 <omonsaputra65@gmail.com>
2024-09-01 19:44:22 +0700

    Kedai-Ucoks

--- src/app.js
+++ src/app.js
@@ -1,26 +1,113 @@
-document.addEventListener('alpine:init', () => {
-    Alpine.data('order', () => ({
-        items: [
-            { id: 1, name: 'Sacoks', img: '1.jpg', price: 8000 },
-            { id: 2, name: 'Ucok Stail', img: '2.jpg', price: 6000 },
-        ],
-    }));
+document.addEventListener("alpine:init", () => {
+  Alpine.data("order", () => ({
+    items: [
+      { id: 1, name: "Sacoks", img: "1.jpg", price: 8000, },
+      { id: 2, name: "Ucok Stail", img: "2.jpg", price: 6000 },
+    ],
+  }));
 
-    Alpine.store("cart", {
-        items: [],
-        total: 0,
-        quantity: 0,
-        add(newItem) {
-            console.log(newItems);
-        }
-    });
+  Alpine.store("cart", {
+    items: [],
+    total: 0,
+    quantity: 0,
+    add(newItem) {
+      // cek barang
+      const cartItem = this.items.find((item) => item.id === newItem.id);
+
+      // belum ada barang
+      if (!cartItem) {
+        this.items.push({ ...newItem, quantity: 1, total: newItem.price });
+        this.quantity++;
+        this.total += newItem.price;
+      } else {
+        this.items = this.items.map((item) => {
+          if (item.id !== newItem.id) {
+            return item;
+          } else {
+            item.quantity++;
+            item.total = item.price * item.quantity;
+            this.quantity++;
+            this.total += item.price;
+            return item;
+          }
+        });
+      }
+    },
+    remove(id) {
+      // remove item mentargetkan ID
+      const cartItem = this.items.find((item) => item.id === id);
+
+      if (cartItem.quantity > 1) {
+        this.items = this.items.map((item) => {
+          if (item.id !== id) {
+            return item;
+          } else {
+            item.quantity--;
+            item.total = item.price * item.quantity;
+            this.quantity--;
+            this.total -= item.price;
+            return item;
+          }
+        });
+      } else if (cartItem.quantity === 1) {
+        this.items = this.items.filter((item) => item.id !== id);
+        this.quantity--;
+        this.total -= cartItem.price;
+      }
+    },
+  });
 });
+
+// form validasi
+const checkoutButton = document.querySelector(".checkout-button");
+checkoutButton.disabled = true;
+
+const form = document.querySelector("#checkoutForm");
+
+form.addEventListener("keyup", function () {
+  for (let i = 0; i < form.elements.length; i++) {
+    if (form.elements[i].value.length !== 0) {
+      checkoutButton.classList.remove("disabled");
+      checkoutButton.classList.add("disabled");
+    } else {
+      return false;
+    }
+  }
+  checkoutButton.disabled = false;
+  checkoutButton.classList.remove("disabled");
+});
+
+// kirim data tombol checkout
+checkoutButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const data = new URLSearchParams(formData);
+  const objData = Object.fromEntries(data);
+  const message = formatMessage(objData);
+  window.open("http://wa.me/628989741162?text=" + encodeURIComponent(message));
+});
+
+
+//format pesan WhatsApp
+const formatMessage = (obj) => {
+  return `Data Customer
+      Nama: ${obj.name}
+      Kelas: ${obj.class}
+      Email: ${obj.email}
+      No HP: ${obj.phone}
+Data Pesanan
+  ${JSON.parse(obj.items).map(
+    (item) => `${item.name} (${item.quantity} x ${rupiah(item.total)}) \n`
+  )}
+  TOTAL: ${rupiah(obj.total)}
+  Terima kasih.`;
+};
 
 // konversi ke rupiah
 const rupiah = (number) => {
-    return new Intl.NumberFormat("id-ID", {
-      style: "currency",
-      currency: "IDR",
-      minimumFractionDigits: 0
-    }).format(number);
-  };
+  return new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+    minimumFractionDigits: 0,
+  }).format(number);
+};
